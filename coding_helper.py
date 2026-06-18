1e4277a59b0a6a467bda2cb6efaef7dfab7c600f
Kanik Sharma <kaniksharma886@gmail.com>
2026-06-18 12:33:11 +1000

    Task 3.4 - coding helper

--- coding_helper.py
+++ coding_helper.py
@@ -1,3 +1,4 @@
+from langchain_core.messages import HumanMessage, SystemMessage
 from langchain.chat_models import init_chat_model
 from pydantic import BaseModel, Field
 import prompt_config as pconfig
@@ -19,13 +20,6 @@ def run_code(code: str) -> str:
     
 
 def create_code(user_prompt, past_errors=None):
-    
-    try:
-        retriever = rag_manager.get_retriever()
-    except Exception as e:
-        utils.log(f"Error: Failed to get retriever: {e}")
-        return
-
 
     try:
         model = init_chat_model(config.MODEL_NAME, temperature=config.MODEL_TEMPERATURE, base_url=config.OPENAI_BASE_URL)
@@ -33,7 +27,7 @@ def create_code(user_prompt, past_errors=None):
         
     except Exception as e:
         utils.log(f"Error : Failed to load model {config.MODEL_NAME}': {e}")
-        return
+        return None
 
     conversation_history = [SystemMessage(content=pconfig.SYSTEM_MESSAGE)]
     conversation_history.append(HumanMessage(content=user_prompt))
@@ -41,14 +35,18 @@ def create_code(user_prompt, past_errors=None):
         error_prompt = f"""Previous run resulted in these errors: 
         {past_errors}"""
         conversation_history.append(HumanMessage(content=error_prompt))
-    
+
+    final_structured_json = ""
     try:            
         code = model.invoke(conversation_history)
-        utils.log(f"\nCode: {code}")
-        final_structured_json = model_with_structure.invoke(f"Based on your response for the prompt '{user_prompt}', format the final response: {code['output']}") 
+        utils.log(f"\nCode: {code.content}")
+        format_prompt = f"Based on your response for the prompt '{user_prompt}', format the final response: {code.content}"
+        print(format_prompt)
+        final_structured_json = model_with_structure.invoke(format_prompt) 
     except Exception as e:
         utils.log(f"Error: Response failed : {e}")
-    
+    return final_structured_json
+
 
 if __name__ == "__main__":
     prompt = "Write code to add 4 numbers in python"
